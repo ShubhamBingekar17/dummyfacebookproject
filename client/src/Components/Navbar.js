63962d227266da12b78318ff6d15e290beda3a3a
Shubham Bingekar <75237471+ShubhamBingekar17@users.noreply.github.com>
2022-05-19 11:36:03 +0530

    navbar

--- client/src/Components/Navbar.js
+++ client/src/Components/Navbar.js
@@ -10,13 +10,13 @@ const Navbar = () => {
     const [user, setUser] = useState([]);
 
     const getUser = () => {
-      // fetch('https://dummyfacebook.herokuapp.com/post/user')
-      //   .then((response) => {
-      //     return response.json();
-      //   }).then((data) => {
-      //     setUser(data)
-      //     return
-      //   })
+      fetch('https://dummyfacebook.herokuapp.com/post/user')
+        .then((response) => {
+          return response.json();
+        }).then((data) => {
+          setUser(data)
+          return
+        })
     }
     getUser()
     
@@ -24,17 +24,19 @@ const Navbar = () => {
   return (
     <Stack direction='row' className='navbar' mt={1} p={1} justifyContent={'space-between'}>
         <Stack spacing={1} direction='row' ml={4}>
+        
+        <Input placeholder='Search' value='' maxWidth={300}  borderRadius={50}/>
         <IconButton
             colorScheme='blue'
             aria-label='Search database'
             icon={<SearchIcon />}
             />
-        <Input placeholder='Search' value='' maxWidth={300}  borderRadius={50}/>
         </Stack>
         <Stack spacing={1} direction='row' me={10}>
         <img className='profile_img_main' color='black' src={user[0] ? user[0].profile : profile} alt='' borderRadius='50%' />
         <Text size='xl' value='' display={window.innerWidth < 600? 'none': 'block'} p={2}>{user[0] ? user[0].name : 'Auto Loging...'}</Text>
         <IconButton 
+        
         aria-label='Search database' 
         icon={<img color='black' src={messenger} alt='' />}  
         isRound={true}
